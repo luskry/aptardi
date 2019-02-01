55e7b9f0c489776c2db754bfb05561baa6e0a514
Trape <ryan.lusk@ucdenver.edu>
2019-01-31 19:07:54 -0700

    Add files via upload

---
+++ signal_variant_indicator_1-31-19.py
@@ -0,0 +1,9 @@
+def signal_variant_indicator(bases, signal_variants):
+
+    hex_seq_list = []
+    for i in range(len(bases) - 5):
+        hex_seq_list.append(bases[i:i + 6])
+    if any(sig in signal_variants for sig in hex_seq_list):
+        return 1
+    else:
+        return 0
