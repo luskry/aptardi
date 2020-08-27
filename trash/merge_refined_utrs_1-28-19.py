7d4cd80f1a15c030b8ba9e34f93f18c6756a1656
Trape <ryan.lusk@ucdenver.edu>
2020-08-27 16:24:42 -0600

    Delete merge_refined_utrs_1-28-19.py

--- trash/merge_refined_utrs_1-28-19.py
+++
@@ -1,8 +0,0 @@
-def merge_refined_utrs(tmp_dir, refined_utr_file, merged_refined_utr_file):
-
-    os.system('sort -k1,1 -k2,2n %s | bedtools merge -i - -s -c 4,5,6 -o collapse,distinct,distinct -delim "/" > %s' % (tmp_dir+refined_utr_file, tmp_dir+merged_refined_utr_file))
-    
-    try:
-        os.remove(tmp_dir+refined_utr_file)
-    except OSError:
-        pass
