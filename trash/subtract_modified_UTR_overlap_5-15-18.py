a3556011ecac930675f066a0d558b05f4b11bdcf
Trape <ryan.lusk@ucdenver.edu>
2020-08-27 16:26:39 -0600

    Delete subtract_modified_UTR_overlap_5-15-18.py

--- trash/subtract_modified_UTR_overlap_5-15-18.py
+++
@@ -1,8 +0,0 @@
-def subtract_modified_UTR_overlap(UTR_bed_path, tmp_dir):
-    
-    os.system('sort -k1,1 -k2,2n -k3,3n -k6,6 -u %s > %s' % (tmp_dir+UTR_bed_path, tmp_dir+'UTR_duplicate_subtracted.bed'))
-    
-    try:
-        os.remove(tmp_dir+UTR_bed_path)
-    except OSError:
-        pass
