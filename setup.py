e0f3d9667fe67ab96aba73f85d63646115108e7c
Trape <ryan.lusk@ucdenver.edu>
2021-05-14 18:28:40 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='aptardi',
+      version='1.3.4',
+      description='Identify polyA sites',
+      author='Ryan Lusk',
+      author_email='ryan.lusk@cuanschutz.edu',
+      url='https://github.com/luskry/aptardi',
+      packages=['aptardi'],
+      package_dir={'aptardi': 'src/'},
+      package_data={'aptardi': ['ml_scale/model.hdf5', 'ml_scale/scale.pk']}
+      entry_points={'console_scripts': ['aptardi = src.aptardi:main']})
