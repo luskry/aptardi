63f03c6a09c3d84c34a097e806677a8ff2e3e727
Trape <ryan.lusk@ucdenver.edu>
2021-05-18 13:55:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='aptardi',
-      version='1.3.8',
+      version='1.3.9',
       description='Identify polyA sites',
       author='Ryan Lusk',
       author_email='ryan.lusk@cuanschutz.edu',
       url='https://github.com/luskry/aptardi',
       packages=['aptardi'],
-      package_dir={'aptardi': 'src/'},
+      package_dir={'aptardi': 'src/aptardi'},
       package_data={'aptardi': ['ml_scale/model.hdf5', 'ml_scale/scale.pk']},
-      entry_points={'console_scripts': ['aptardi = src.aptardi']})
+      scripts=['src/aptardi/scripts/aptardi'])
