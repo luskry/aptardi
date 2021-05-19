15248f4c85369f5ba283fd7e96b4a205b29f828c
Trape <ryan.lusk@ucdenver.edu>
2021-05-19 08:18:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='aptardi',
-      version='1.3.10',
+      version='1.3.11',
       description='Identify polyA sites',
       author='Ryan Lusk',
       author_email='ryan.lusk@cuanschutz.edu',
